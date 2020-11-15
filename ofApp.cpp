83147451f64a22f360f37fd590e7353f8ef17dec
Brian L <brianla7755@gmail.com>
2020-11-14 16:45:12 -0800

    update pt 2
    added new emitters

--- ofApp.cpp
+++ ofApp.cpp
@@ -5,24 +5,79 @@
 //
 // Basic Sprite Object
 //
-
-
-Sprite::Sprite() {
+Sprite::Sprite(bool p, float id) {
 	velocity = glm::vec3(0, 0, 0);		//default velocity upward
-	lifespan = 0;      // lifespan of -1 => immortal 
+	life = 0;      // lifespan of -1 => immortal 
 	birthtime = 0;	//time at creation	
-	haveImage = image.load("images/Beta_Missile.png");
-	haveSound = spriteSound.load("sounds/missile.wav");
+	speed = 0;
+	playerSprite = p;		//set to an emitter
+	ID = id;
+	
+	if (p == true) {
+		if (img.load("images/Beta_Missile.png"))
+			haveImg = true;
+		if (snd.load("sounds/missile.mp3"))
+			haveSnd = true;
+	}
+	else {
+		if (img.load("images/Beta_Missile_2.png"))
+			haveImg = true;
+		if (snd.load("sounds/missile2.mp3"))
+			haveSnd = true;
+	}
+	
+	//else load enemy sprites...
 
 	//default parameters
-	width = 20;
-	height = 20;
+	if (haveImg) {
+		width = img.getWidth();
+		height = img.getHeight();
+	}
+	else {
+		width = 20;
+		height = 20;
+	}
 }
 
 // Return a sprite's age in milliseconds
 //
 float Sprite::age() {
 	return (ofGetElapsedTimeMillis() - birthtime);
+}
+
+//Return an adjusted currPos within SpriteSystem update
+glm::vec3 Sprite::move(bool b, float p) {
+	if (b == true) {
+		return currPos + velocity / ofGetFrameRate();		//if player, continuous vertical display
+	}
+	else {
+		if (p == 1) {
+			currPos.x += speed;
+			return curveEval(currPos.x, scale, cycle, yVal, p);		//else, change positions
+		}
+		else {
+			currPos.x -= speed;
+			return curveEval(currPos.x, scale, cycle, yVal, p);
+		}
+	}
+}
+
+//--------------------------------------------------------------
+// Given x in pixel coordinates, return (x, y, z) on the sin wave
+// Note that "z" is not used, so it is set to "0".
+//
+// Additional Parameters
+//    scale - scales the curve in Y  (the amplitude)
+//    cycles - number of cycles in sin wave.
+//
+glm::vec3 Sprite::curveEval(float x, float scale, float cycles, float y, float det)
+{
+	// x is in screen coordinates [0, WindowWidth]
+	float u = (cycles * x * PI) / ofGetWidth();
+	if (det == 1)				//adjust movement based on "det" value
+		return (glm::vec3(x, -scale * sin(u) + y, 0));
+	else
+		return(glm::vec3(x, -scale * cos(u) + y, 0));
 }
 
 
@@ -34,24 +89,28 @@ void Sprite::draw() {
 
 	// draw image centered and add in translation amount
 	//
-	if (haveImage) {
-		width = image.getWidth();
-		height = image.getHeight();
-
-		image.draw(-width / 2.0 + currPos.x, -height / 2.0 + currPos.y);
-	}
-	else {
+	if (haveImg)
+		img.draw(currPos.x, currPos.y);
+	else 
 		// in case no image is supplied, draw something.
 		// 
 		ofDrawRectangle(-width / 2.0 + currPos.x, 
 			-height / 2.0 + currPos.y, width, height);
-	}
 }
 
 //  Add a Sprite to the Sprite System
 //
 void SpriteSystem::add(Sprite s) {
 	sprites.push_back(s);
+}
+
+
+// Remove a sprite from the sprite system. Note that this function is not currently
+// used. The typical case is that sprites automatically get removed when the reach
+// their lifespan.
+//
+void SpriteSystem::remove(int i) {
+	sprites.erase(sprites.begin() + i);
 }
 
 
@@ -70,17 +129,17 @@ void SpriteSystem::update() {
 	// traversing at the same time, use an iterator.
 	//
 	while (s != sprites.end()) {
-		if (s->age() > s->lifespan) {
+		if (s->age() > s->life) {
 			tmp = sprites.erase(s);
 			s = tmp;
 		}
-		else s++;
+		else s++;	
 	}
 
 	//  Move sprite
 	//
 	for (int i = 0; i < sprites.size(); i++) {
-		sprites[i].currPos += sprites[i].velocity / ofGetFrameRate();
+		sprites[i].currPos = sprites[i].move(sprites[i].playerSprite, sprites[i].ID);		//adjust currPos based on emitter
 	}
 }
 
@@ -92,18 +151,24 @@ void SpriteSystem::draw() {
 	}
 }
 
-//  Create a new Emitter - needs a SpriteSystem
+//  Create a new Emitter - equipped w/ SpriteSystem (PLAYER)
 //
 Emitter::Emitter(SpriteSystem *spriteSys) {
+	isPlayer = true;
+	//------------------------------------------
 	sys = spriteSys;	//personal sprite system
 	speed = 5;		//default speed
-	lifespan = 0;    //projectile lifespan in ms
+	lifespan = 2000;    //projectile lifespan in ms
 	currPos = ofVec3f(ofGetWidth() / 2.0, ofGetHeight() / 2.0, 0);		//default center
-
+	scale = 0;
+	cycle = 0;
+	
 	lastSpawned = 0;
-	rate = 1;    // sprites/sec
-	haveImage = image.load("images/Beta_Ship.png");	//return t/f based on img existence
-	velocity = glm::vec3(0, 0, 0);
+	rate = 3;    // sprites/sec
+	velocity = glm::vec3(0, -500, 0);			//default projectiles
+
+	if (image.load("images/Beta_Ship.png"))
+		haveImage = true;	//return t/f based on img existence
 
 	if (haveImage) {
 		width = image.getWidth();
@@ -115,6 +180,40 @@ Emitter::Emitter(SpriteSystem *spriteSys) {
 	}
 }
 
+//Create a new Emitter - equipped w/ SpriteSystem (ENEMY)
+//
+Emitter::Emitter(SpriteSystem *spriteSys, float id) {
+	isPlayer = false;				//SET TO FALSE TO DENOTE ENEMY
+	cycle = 4;
+	scale = 200;
+	ID = id;
+	//-----------------------------------------------------------
+
+	sys = spriteSys;	//personal sprite system
+	speed = 4;		//default speed
+	lifespan = 5;    //projectile lifespan in ms
+	if (ID == 1)
+		currPos = ofVec3f(0, 300, 0);		//default position
+	else
+		currPos = ofVec3f(ofGetWidth(), 300, 0);
+
+	lastSpawned = 0;
+	rate = 3;    // sprites/sec
+	velocity = glm::vec3(0, 0, 0);			//default projectiles
+
+	//haveImage = image.load("images/Beta_Ship.png");	//return t/f based on img existence
+	if (haveImage) {
+		width = image.getWidth();
+		height = image.getHeight();
+	}
+	else {
+		width = 50;		//50x50 rect replacement if necc.
+		height = 50;
+	}
+}
+
+
+
 //  Update the Emitter. If it has been started, spawn new sprites with
 //  initial velocity, lifespan, birthtime.
 //
@@ -123,31 +222,41 @@ void Emitter::update() {
 	//iterate through SpriteSystem, delete or move existing sprites
 	sys->update();
 
-	if (!ofGetKeyPressed(' ')) return;		//exit if space not pressed
+	if(isPlayer)
+		if (!ofGetKeyPressed(' ')) return;		//exit if space not pressed
 
-	float time = ofGetElapsedTimeMillis();	//current time
-	//check elapsed time; wait until spawning next sprite
-	//based on rate
-	//
+	//current time-------------------
+	//check elapsed time; wait until spawning next sprite based on rate
+	float time = ofGetElapsedTimeMillis();
+
 	if ((time - lastSpawned) > (1000.0 / rate)) {
 		// spawn a new sprite
-		Sprite sprite;
-		if (sprite.haveSound) {
-			sprite.spriteSound.setVolume(0.5f);
-			sprite.spriteSound.play();
+		Sprite sprite(isPlayer, ID);
+
+		if (sprite.haveSnd) {
+			sprite.snd.setVolume(0.3f);
+			sprite.snd.play();
 		}
 		sprite.velocity = velocity;
-		sprite.lifespan = lifespan;
-		sprite.currPos = (!haveImage) ? currPos : ofVec3f(currPos.x, currPos.y + 100, currPos.z);	//currPos adjustment based on img presence
+		sprite.life = lifespan;
+		sprite.currPos = currPos;
+		sprite.yVal = yVal;
 		sprite.birthtime = time;
+		sprite.scale = scale;
+		sprite.cycle = cycle;
+		sprite.speed = speed;
 		sys->add(sprite);
 		lastSpawned = time;
 	}
+
 }
 
 //  Draw the Emitter if it is drawable. In many cases you would want a hidden emitter
 //
 void Emitter::draw() {
+	// draw sprite system
+	//
+	sys->draw();
 
 	//center the image/rect around position vector
 	//check if image is applicable...
@@ -156,23 +265,15 @@ void Emitter::draw() {
 		width = image.getWidth();
 		height = image.getHeight();
 
-
 		//bound player movement
-		if (currPos.y <= 0)
-			image.draw(-width / 2.0 + currPos.x, height / 2.0);
-		else if (currPos.y >= ofGetHeight() / 2.0)
-			image.draw(-width / 2.0 + currPos.x, height / 2.0 + ofGetHeight() / 2.0);
-		else
-			image.draw(-width / 2.0 + currPos.x, height / 2.0 + currPos.y);
+		if (currPos.y >= ofGetWindowHeight() - 200)
+			currPos.y = ofGetWindowHeight() - 200;
+		
+		image.draw(currPos.x, currPos.y);
 	}
 	else {
-		ofSetColor(0, 0, 0);
-		ofDrawRectangle(-width / 2 + currPos.x, -height / 2 + currPos.y, width, height);
+		ofDrawRectangle(-width / 2.0 + currPos.x, -height / 2.0 + currPos.y, width, height);
 	}
-
-	// draw sprite system
-	//
-	sys->draw();
 }
 
 // Set rate of fire
@@ -185,49 +286,77 @@ void Emitter::setLifeSpan(float hp) {
 	lifespan = hp;
 }
 
+
 void Emitter::setSpeed(float s) {
 	speed = s;
 }
 
-//adjust velocity vector and magnitude
+/*
 void Emitter::setVelocity(ofVec3f v) {
 	velocity = v;
 }
+*/
+
+
 
 //--------------------------------------------------------------
 void ofApp::setup(){
 
 	//ofSetVerticalSync(true);
 	ofSetBackgroundColor(ofColor::black);
-
-	// create an image for sprites being spawned by emitter
-	//
-	if (defaultImage.load("images/space-invaders-ship-scaled.png")) {
-		imageLoaded = true;
-	}
-	else {
-		ofLogFatalError("can't load image: images/space-invaders-ship-scaled.png");
-		ofExit();
-	}
-
 	
 	gui.setup();
-	gui.add(mouseOn.setup("Mouse", true));		//set to use mouse first
-	gui.add(speedMov.setup("Speed", 5, 5, 20));
-	gui.add(rate.setup("Rate of Fire", 1, 1, 10));
+	//gui.add(mouseOn.setup("Mouse", true));		//set to use mouse first
+	gui.add(yAdjust.setup("LEFT Y", 200, 0, ofGetHeight()));
+	gui.add(yAdjust2.setup("RIGHT Y", 200, 0, ofGetHeight()));
+	gui.add(speedMov.setup("Speed", 4, 1, 10));
+	gui.add(scale.setup("Scale", 200, 1, 400));
+	gui.add(cycles.setup("Cycles", 4, 1, 10));
+	gui.add(rate.setup("Rate of Fire", 3, 1, 10));
 	gui.add(life.setup("life", 5, .5, 10));
-	gui.add(velocity.setup("velocity", ofVec3f(0, -100, 0), ofVec3f(-1000, -1000, -1000), ofVec3f(1000, 1000, 1000)));
+	//gui.add(velocity.setup("velocity", ofVec3f(0, -500, 0), ofVec3f(-1000, -1000, -1000), ofVec3f(1000, 1000, 1000)));
 
 }
 
 //--------------------------------------------------------------
 void ofApp::update() {
 	if (play) {
-		emit->setRate(rate);
-		emit->setLifeSpan(life * 500);
-		emit->setSpeed(speedMov);
-		emit->setVelocity(ofVec3f(velocity->x, velocity->y, velocity->z));
-		emit->update();
+
+		//ENEMY PARAMETERS VIA GUI
+		enemy->setRate(rate);
+		enemy->setLifeSpan(life * 500);
+		enemy->setSpeed(speedMov);
+		enemy->currPos.y = yAdjust;
+		enemy->yVal = yAdjust;
+		enemy->scale = scale;
+		enemy->cycle = cycles;
+
+		enemy2->setRate(rate);
+		enemy2->setLifeSpan(life * 500);
+		enemy2->setSpeed(speedMov);
+		enemy2->currPos.y = yAdjust2;
+		enemy2->yVal = yAdjust2;
+		enemy2->scale = scale;
+		enemy2->cycle = cycles;
+		
+
+		enemy->update();
+		enemy2->update();
+		//player->setVelocity(ofVec3f(velocity->x, velocity->y, velocity->z));
+		player->update();
+
+		for (int i = 0; i < player->sys->sprites.size(); i++) {
+			for (int j = 0; j < enemy->sys->sprites.size(); j++) {
+				if (player->sys->sprites[i].inside(enemy->sys->sprites[j].currPos)) {
+					player->sys->sprites[i].life = 0;			//if within boundaries, reset lifespan of sprites to 0 for deletion
+					enemy->sys->sprites[j].life = 0;
+					playScore++;			//update player 
+					cout << "collision detected" << endl;
+				}
+			}
+		}	//end for loop
+
+
 	}
 }
 
@@ -235,25 +364,24 @@ void ofApp::update() {
 //--------------------------------------------------------------
 void ofApp::draw(){
 
-	//check for start prompt
+	//if in play state...
 	if (play) {
-		emit->draw();
-		gui.draw();
-		
 
-		//draw lines to represent boundaries
+		ofDrawBitmapStringHighlight("SCORE = " + std::to_string(playScore), ofVec3f(ofGetWidth() / 1.5, 100, 0));
+		player->draw();		//draw player
+		enemy->draw();		//draw enemy
+		enemy2->draw();
+		gui.draw();			//draw GUI
+		
 		ofSetColor(ofColor::red);
-		ofDrawLine(0, emit->height / 2.0, ofGetWidth(), emit->height / 2.0);
-		ofDrawLine(0, emit->height * 1.5 + ofGetHeight() / 2.0, 
-			ofGetWidth(), emit->height * 1.5 + ofGetHeight() / 2.0);
+		//draw lines to represent boundaries
+		ofDrawLine(0, ofGetWindowHeight() - 80, ofGetWidth(), ofGetWindowHeight() - 80);
 	}
 	else
-		ofDrawBitmapString("Press Space to start", emit->currPos);		//intro
+		ofDrawBitmapString("Press Space to Start", player->currPos);		//intro prompt otherwise
 
 }
 
-
-//--------------------------------------------------------------
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
@@ -262,14 +390,14 @@ void ofApp::mouseMoved(int x, int y ){
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-	if(mouseOn)
-		emit->currPos = ofVec3f(x, y, 0) - mouse_last;
+	//if(mouseOn)
+	player->currPos = ofVec3f(x, y, 0) - mouse_last;		//parallel player displacement
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-	if(mouseOn)
-		mouse_last = ofVec3f(x, y, 0) - emit->currPos;
+	//if(mouseOn)
+	mouse_last = ofVec3f(x, y, 0) - player->currPos;		//save mouse position
 
 }
 
@@ -288,7 +416,7 @@ void ofApp::mouseExited(int x, int y){
 }
 
 void ofApp::keyPressed(int key) {	
-	if (!mouseOn) {
+	/*if (!mouseOn) {
 		switch (key) {
 		case OF_KEY_UP:
 			emit->currPos.y -= emit->speed;
@@ -305,7 +433,7 @@ void ofApp::keyPressed(int key) {
 		default:
 			break;
 		}
-	}
+	} */
 	
 }
 
@@ -313,10 +441,11 @@ void ofApp::keyPressed(int key) {
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key) {
 	if (!play && key == ' ') {	//if not in game state, enable 
-		if (defaultSound.load("sounds/start.wav"))
+		if (defaultSound.load("sounds/start.wav"))		//play start sound
 			defaultSound.play();
-		ofSetBackgroundColor(ofColor::white);
-		play = true;
+		ofSetBackgroundColor(ofColor::white);		//default BG settings
+		play = true;			//set play state
+
 	}
 }
 
--- ofApp.h
+++ ofApp.h
@@ -3,39 +3,56 @@
 #include "ofMain.h"
 #include "ofxGui.h"
 
-typedef enum { MoveStop, MoveLeft, MoveRight, MoveUp, MoveDown } MoveDir;
+//	typedef enum { MoveStop, MoveLeft, MoveRight, MoveUp, MoveDown } MoveDir;	//unused enumeration
 
 //Base object
 //
 class BaseObject {
 public:
-	BaseObject() {};	//constructor
-	glm::vec3 currPos;		//current position: centered
-	void setPosition(glm::vec3 position) {
-		currPos = position;			//renew position
-	}
+	BaseObject() {};		//constructor
+
+	glm::vec3 currPos = ofVec3f(0, 0, 0);		//current position: centered
+	//float rot;			//rotational val
+	//glm::vec3 scale = glm::vec3(1, 1, 1);
 };
 
 //  General Sprite class  (similar to a Particle)
 //
 class Sprite : public BaseObject {
 public:
-	Sprite();		//constructor
+	Sprite(bool, float);		//constructor
 
 
 	//functions-------------------------------------------------------
 	void draw();		//draw override
 	float age();		//elapsed time after birth
-	glm::vec3 velocity;		//velocity vec (magnitude and direction)
+	glm::vec3 move(bool, float);		//custom move function
+	glm::vec3 curveEval(float, float, float, float, float);		//curveEval func adjusted
+	bool inside(glm::vec3 pos) {
+		if (pos.x >= (currPos.x - width) && pos.x <= (currPos.x + width) &&
+			pos.y >= (currPos.y - height) && pos.y <= (currPos.y + height))
+			return true;
+		else
+			return false;
+	}
+
+
 
 	//variables-------------------------------------------------------
-	ofImage image;		//imbedded image
-	ofSoundPlayer spriteSound;		//sound for missile
+	ofImage img;		//imbedded image
+	ofSoundPlayer snd;		//sound for missile
 	float birthtime; //birth time
-	float lifespan;  //length of span
-	bool haveImage;		//bool flag if image is available
-	bool haveSound;		//bool flag for sound availability
+	float life;  //length of span
+	bool haveImg = false;		//bool flag if image is available
+	bool haveSnd = false;		//bool flag for sound availability
+	bool playerSprite;		//check player sprite
 	float width, height;		//width/height of rect
+	glm::vec3 velocity;		//velocity vec (magnitude and direction)
+
+
+	float speed;			//speed of projectile
+	float scale, cycle, yVal;		//scale, cycle, and yVal storage
+	float ID;			//ID for emitter
 };
 
 //  Manages all Sprites in a system.  You can create multiple systems
@@ -43,10 +60,10 @@ public:
 class SpriteSystem  {
 public:
 	void add(Sprite);		//add sprite to system
+	void remove(int i);
 	void update();		//update system
 	void draw();			//run system
 	vector<Sprite> sprites;		//vector storing sprites - sprite system
-
 };
 
 
@@ -55,7 +72,8 @@ public:
 //
 class Emitter: public BaseObject {
 public:
-	Emitter(SpriteSystem *);		//constructor attached with sprite system
+	Emitter(SpriteSystem *);		//default = player
+	Emitter(SpriteSystem *, float);		//parameterized constructor bool = enemy
 
 	
 	//functions-------------------------------------------------------
@@ -64,19 +82,26 @@ public:
 	void setRate(float);
 	void setLifeSpan(float);
 	void setSpeed(float);
-	void setVelocity(ofVec3f);
+	//void setVelocity(ofVec3f);
 
 
 	//variables-------------------------------------------------------
 	SpriteSystem *sys;		//system instance
 	float rate;			//float rate
-	float speed;		//speed when using arrow keys
+	float speed;		//speed of sprites
 	glm::vec3 velocity;			//veloc
 	float lifespan;			//lifespan of entire emitter/system	
 	float lastSpawned;		//last projectile spawned
 	ofImage image;		//imbedded image
-	bool haveImage;		//bool switch for rect or image
+	bool haveImage = false;		//bool switch for rect or image
 	float width, height;		//width/height for rect
+
+	float scale, cycle;			//scale cycle default
+	float yVal = 0;
+	float ID = 0;
+
+	//check if player-------------------------------------------------
+	bool isPlayer;			//check if player
 };
 
 
@@ -99,21 +124,29 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 		
-		Emitter *emit = new Emitter(new SpriteSystem);
+		Emitter *player = new Emitter(new SpriteSystem);			//player emitter
+		Emitter *enemy = new Emitter(new SpriteSystem, 1);			//enemy
+		Emitter *enemy2 = new Emitter(new SpriteSystem, 2);
+		int playScore = 0;					//tally score of player
 
 		ofImage defaultImage;
 		ofSoundPlayer defaultSound;		//begin sound
 		glm::vec3 mouse_last;		//last mouse position
-		bool imageLoaded;		//image check
+		bool imageLoaded = false;			//image check
 
-		bool bHide;
+		//bool bHide;
 		bool play = false;			//bool start switch
 
+		//sliders & buttons------------------------------------------
+		ofxFloatSlider scale;		//scale of sin wave
+		ofxFloatSlider cycles;		//cycles of sin wave
 		ofxFloatSlider rate;
 		ofxFloatSlider life;
 		ofxVec3Slider velocity;
-		ofxToggle mouseOn;
+		//ofxToggle mouseOn;		//temporarily disable keystrokes
 		ofxFloatSlider speedMov;
+		ofxFloatSlider yAdjust;		//adjust y placement of enemy emitter spawn
+		ofxFloatSlider yAdjust2;
 		//ofxLabel screenSize;
 
 
